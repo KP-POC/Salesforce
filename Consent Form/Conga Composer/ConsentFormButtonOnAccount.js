18eaeca779801105237549f6fdb75d0c013a6ccb
Vuk Dukic <vuk.dukic@clara.com>
2018-02-23 12:47:36 -0800

    Update ConsentFormButtonOnAccount.js

--- Consent Form/Conga Composer/ConsentFormButtonOnAccount.js
+++ Consent Form/Conga Composer/ConsentFormButtonOnAccount.js
@@ -2,3 +2,8 @@
 ?serverUrl={!API.Partner_Server_URL_370}
 &id={!Account.Id}
 &SF1=1
+
+https://composer.congamerge.com
+?sessionId={!API.Session_ID}
+&serverUrl={!API.Partner_Server_URL_290}
+&id={!Opportunity.Id}
