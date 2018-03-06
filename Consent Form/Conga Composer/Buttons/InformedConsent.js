cbd7254fc7dc30a273ad13e7ffedabc7faf842c3
Vuk Dukic <vuk.dukic@clara.com>
2018-03-06 14:20:09 -0800

    Create InformedConsent.js

---
+++ Consent Form/Conga Composer/Buttons/InformedConsent.js
@@ -0,0 +1,16 @@
+/apex/APXTConga4__Conga_Composer
+&serverUrl={!API.Partner_Server_URL_80}
+&Id={!Account.Id}
+&TemplateID=a0Lq0000006yNRD
+&DocuSignVisible=1
+&DocuSignR1Id={!Account.Name}
+&DocuSignR1Type=Signer
+&DocuSingR1Role=Signer+1
+&DocuSignR1RoutingOrder=1
+
+&DocuSignR2TypeId={!Contact.Id}
+&DocuSignR2Role=Signer+2
+&DocuSignRoutingOrder=2
+
+&DS7=17
+&SF1=1
