e41fd637b9eaea02b1fcf0c0eb1319f1ad2b19f8
Vuk Dukic <vuk.dukic@clara.com>
2018-03-06 07:29:18 -0800

    Update MergeTemplate.js

--- Consent Form/Conga Composer/MergeTemplate.js
+++ Consent Form/Conga Composer/MergeTemplate.js
@@ -2,3 +2,14 @@ https://composer.congamerge.com/?SessionID={!API.Session_ID}
 &ServerURL={!API.Partner_Server_URL_290}
 &Id={!Contact.Id}
 &TemplateId=015q00000007CwL
+
+apex/APXTConga4__Conga_Composer
+?serverUrl={!API.Partner_Server_URL_370}
+&id={!Account.Id}
+&SF1=1
+
+apex/APXTConga4__Conga_Composer
+?serverUrl={!API.Partner_Server_URL_290}
+&id={!Contact.Id}
+&SF1=1
+&TemplateId=015q00000007CwL
