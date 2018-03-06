23cba519941f236b466edc76ac878abb4c2e04fc
Vuk Dukic <vuk.dukic@clara.com>
2018-03-06 12:31:46 -0800

    Create Composer With DocuSign for 2 Signers.js

---
+++ Consent Form/Conga Composer/Buttons/Composer With DocuSign for 2 Signers.js
@@ -0,0 +1,9 @@
+https://composer.congamerge.com
+?sessionId={!API.Session__ID}
+&serverUrl={!API.Partner_Server_URL_80}
+&Id={!Account.Id}
+&TemplateID=
+&DocuSignVisible=1
+&DocuSignR1Id=  //Who will be the 1st recipient of the DS envelope: Contact, Lead of User.
+                // Merge Saleforce fields, Contact Roles or Advanced Options
+
