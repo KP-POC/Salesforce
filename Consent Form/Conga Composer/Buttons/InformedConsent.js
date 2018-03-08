ffb4471d5385756d73971bcbcaeaee944e92b741
Vuk Dukic <vuk.dukic@clara.com>
2018-03-08 09:36:03 -0800

    Update InformedConsent.js

--- Consent Form/Conga Composer/Buttons/InformedConsent.js
+++ Consent Form/Conga Composer/Buttons/InformedConsent.js
@@ -19,4 +19,10 @@
 the parameter &docusignr1id is being passed text, but it is looking for a SF id. 
 Do you have a user id or contact id you can pass it? Or even a field like Owner Id? 
 You will want to do the same for the second signer too, if it is predefined.
+
+
+The name can only contain underscores and alphanumeric characters. 
+It must begin with a letter and be unique, and must not include spaces, 
+end with an underscore, or contain two consecutive underscores. 
+
 */
