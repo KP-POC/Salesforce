a2bf38367906523670c1f9e0426ce91aa5fe69f1
Vuk Dukic <vuk.dukic@clara.com>
2018-03-08 11:16:02 -0800

    Update InformedConsent.js

--- Consent Form/Conga Composer/Buttons/InformedConsent.js
+++ Consent Form/Conga Composer/Buttons/InformedConsent.js
@@ -1,7 +1,7 @@
 /apex/APXTConga4__Conga_Composer
 &serverUrl={!API.Partner_Server_URL_80}
 &Id={!Account.Id}
-&TemplateID=a0Lq0000006yPzm
+&TemplateID=a0Lq0000006yPzw
 &DocuSignVisible=1
 &DocuSignR1Id={!Contact.Id}
 &DocuSignR1Type=Signer
@@ -16,6 +16,10 @@
 &SF1=1
 
 /*
+Attached file = 00Pq0000007JZv2
+Conga template id = a0Lq0000006yPzw
+
+
 the parameter &docusignr1id is being passed text, but it is looking for a SF id. 
 Do you have a user id or contact id you can pass it? Or even a field like Owner Id? 
 You will want to do the same for the second signer too, if it is predefined.
