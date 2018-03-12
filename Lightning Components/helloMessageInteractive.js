746492245c2bd98830fed25d6b4813072f579061
Vuk Dukic <vuk.dukic@clara.com>
2018-03-12 09:29:20 -0700

    Update helloMessageInteractive.js

--- Lightning Components/helloMessageInteractive.js
+++ Lightning Components/helloMessageInteractive.js
@@ -1,3 +1,10 @@
+/*
+When the button is clicked, its action handler gets called (1).
+In the action handler, the controller gets the button that was clicked, pulls the label text out of it, 
+and then sets the component’s message attribute to that text (2). 
+And the message of the day is updated (3).
+*/
+
 ({
     handleClick: function(component, event, helper) {
         var btnClicked = event.getSource();         // the button
