5e1852b41cdc12429e7bd818bf8059345545723b
Vuk Djukic <vuk.dukic@clara.com>
2018-03-15 07:50:42 -0700

    Update gotoURL-handler.js

--- SMART/gotoURL-handler.js
+++ SMART/gotoURL-handler.js
@@ -4,5 +4,14 @@
       urlEvent.setParams({
         "url" : "/006/o"
       });
-  } 
+      urlEvent.fire();
+  },
+  navigate: function(component) {
+      var address = '/Salesforce.com+Inc/@37.793779,-122.39448,17z/';
+      var urlEvent = $A.get("e.force:navigateToURL");
+      urlEvent.setParams({
+        "url" : 'https://www.google.com/maps/place/' + address
+      });
+      urlEvent.fire();
+  }
 )}
