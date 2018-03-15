35a7465389af0d7b1e7376b01d211675d7a4db60
Vuk Djukic <vuk.dukic@clara.com>
2018-03-15 07:37:22 -0700

    Update gotoURL-handler.js

--- SMART/gotoURL-handler.js
+++ SMART/gotoURL-handler.js
@@ -1,5 +1,8 @@
 {(
   gotoURL: function(component) {
-      var urlEvent = $A.get("e.
+      var urlEvent = $A.get("e.force:navigateToURL");
+      urlEvent.setParams({
+        "url" : "/006/o"
+      });
   } 
 )}
