77b3e8efe8fc4e519a7ee9f07024cd3fc636407c
Vuk Djukic <vuk.dukic@clara.com>
2018-03-16 11:41:06 -0700

    Update gotoURL-controller.js

--- SMART/gotoURL-controller.js
+++ SMART/gotoURL-controller.js
@@ -1,8 +1,8 @@
-{(
+({
   gotoURL: function(component, event, helper) {
       helper.gotoURL(component);
   },
   navigate: function(component, event, helper) {
       helper.navigate(component);
   }    
-)}
+})
