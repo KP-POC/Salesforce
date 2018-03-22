8e3c32e5a619baa1093b4504ace29b1c9b55c11f
Vuk Djukic <vuk.dukic@clara.com>
2018-03-22 12:05:56 -0700

    Update Controller.js

--- SMART/Button - redirect to Disclosure Page/Controller.js
+++ SMART/Button - redirect to Disclosure Page/Controller.js
@@ -1,7 +1,7 @@
 ({
 	gotoURL : function (component) {    
     $A.get("e.force:navigateToURL").setParams({ 
-       "url": "https://cfsandbox-fhir-app.cs21.force.com/SMART/s/profile/0051N000005sozu" 
+       "url": "https://cfsandbox-fhir-app.cs21.force.com/s/disclaimer" 
     }).fire();
 }
 })
