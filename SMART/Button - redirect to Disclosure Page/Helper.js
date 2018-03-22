10948060232ac16f79100fdac82882cf9dbe4e01
Vuk Djukic <vuk.dukic@clara.com>
2018-03-22 12:06:26 -0700

    Update Helper.js

--- SMART/Button - redirect to Disclosure Page/Helper.js
+++ SMART/Button - redirect to Disclosure Page/Helper.js
@@ -2,7 +2,7 @@
     gotoURL : function (component) {
         var urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-          "url": "one/one.app#/home"
+          "url": "https://cfsandbox-fhir-app.cs21.force.com/s/disclaimer"
         });
         urlEvent.fire();
     }
